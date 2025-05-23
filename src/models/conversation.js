68ba2f4106d157981decb42b853036e39c5a36a7
funmi <sannifunmilola26@gmail.com>
2025-05-23 13:09:58 +0100

    project

--- src/models/conversation.js
+++ src/models/conversation.js
@@ -1,46 +1,31 @@
 const mongoose = require('mongoose');
-const { v4: uuidv4 } = require('uuid'); 
+const { v4: uuidv4 } = require('uuid');
 
 const conversationSchema = new mongoose.Schema({
-    conversationId: {
-        type: String,
-        unique: true,
-        default: uuidv4,
-        index: true
-    },
-    host: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
-    },
-    participants: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
-    }],
-    created_time: {
-        type: Date,
-        default: Date.now,
-        immutable: true
-    },
-    last_updated: {
-        type: Date,
-        default: Date.now
-    },
-    message_list: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Message',
-        required: true
-    }]
+  conversationId: {
+    type: String,
+    unique: true,
+    default: uuidv4,
+    index: true
+  },
+  host: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  participants: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }],
+  messages: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Message'
+  }]
 }, {
-    timestamps: true
+  timestamps: true  
 });
 
-conversationSchema.index({ conversationId: 1, 'participants': 1 });
-
-conversationSchema.pre('save', function(next) {
-    this.last_updated = new Date();
-    next();
-});
+conversationSchema.index({ conversationId: 1, participants: 1 });
 
 module.exports = mongoose.model('Conversation', conversationSchema);
